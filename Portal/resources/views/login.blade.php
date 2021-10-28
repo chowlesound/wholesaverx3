9603cc97fb8ad5fdaf677bfe8a0864a72aa690f5
chowlesound <81609255+chowlesound@users.noreply.github.com>
2021-10-28 19:21:07 -0400

    v2.0

---
+++ Portal/resources/views/login.blade.php
@@ -0,0 +1,18 @@
+@extends('master')
+@section('content')
+
+@endsection
+
+<div class="container-fluid">
+  <div class="row py-4">
+    <div class="col" align="center">
+    <img class="img-fluid" src="https://portal.wholesaverx.com/home_page_images/83137302c012bc29ae541fe78e45b714.png" class="site_logo">
+    </div>
+  </div>
+  <div class="row py-4">
+    <div class="col" align="center">
+<button class="btn btn-primary">Create Account</button>
+<button class="btn btn-primary">Login</button>
+    </div>
+  </div>
+</div>
---
+++ Portal/resources/views/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Portal</title>
+
+    <!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <!-- Bootstrap | Popper | Fontawesome -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+
+  </body>
+</html>
